acb43c1ea18f29641df1129c473cdf84d103bd15
konojack <konojack@gmail.com>
2022-04-13 14:02:07 +0200

    creating a basic reducer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,20 @@
+interface RepositoriesState {
+  data: string[];
+  loading: boolean;
+  error: string | null;
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: [] };
+    case 'search_repositories_success':
+      return { loading: false, error: null, data: action.payload };
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
