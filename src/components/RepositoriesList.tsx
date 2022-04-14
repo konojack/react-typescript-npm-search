2fa8372fdc25b9567b318b4cde6bf3f2670027ea
konojack <konojack@gmail.com>
2022-04-14 15:09:46 +0200

    RepositoriesList build

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState<string>('chuj');
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onFormSubmit}>
+        <input
+          value={term}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTerm(e.target.value)
+          }
+        />
         <button>Search</button>
       </form>
     </div>
