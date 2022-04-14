084bc069f72c0dc58260269f635f2ba068aae386
konojack <konojack@gmail.com>
2022-04-14 14:44:21 +0200

    prepare the view

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import RepositoriesList from './RepositoriesList';
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <div>
+        <h1>Search for repositories</h1>
+      </div>
+      <RepositoriesList />
+    </>
+  );
+};
+
+export default App;
