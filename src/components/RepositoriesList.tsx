45488bc9215d83cb16570a7a675a3a1b14447c91
konojack <konojack@gmail.com>
2022-04-14 16:53:21 +0200

    displaying data

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { useActions } from '../hooks/use-actions';
+import { useSelector } from '../hooks/use-typed-selector';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState<string>('chuj');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector(state => state.repositories);
 
   const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -21,6 +23,9 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map(name => <div key={name}>{name}</div>)}
     </div>
   );
 };
